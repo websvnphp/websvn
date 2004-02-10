da9153e67c8b48380234f3e71b0c3d219296bf5d
tarmes <tarmes@fr.imaje.com>
2004-02-10 09:24:21 +0000

    Extraction of remaining language strings Preparation for new language files

--- templates/header.php
+++ templates/header.php
@@ -2,11 +2,11 @@
    <head>
 		<meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
 	   <link href="templates/styles.css" rel="stylesheet" media="screen">
-      <title>SVN View</title>
+      <title>WebSVN</title>
    </head>
    <body>
       <center>
-         <h1>Subversion Server</h1>
+         <h1><?= $lang["SERVER"]; ?></h1>
       </center>
       <hr>
       <p>
