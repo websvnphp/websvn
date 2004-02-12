ec8cdbe5034b9680c5a6ba823b7201528391ec80
tarmes <tarmes@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2004-02-12 11:21:35 +0000

    ADD: Comprehensive templating solution ADD: Show the age of a revision in the log view
    CHANGE: The youngest revision of the current directory is now shown by
            default (as opposed to the head revision of the entire repository.
            This means that clicking on a directory will show the lastest 
            changes associated with it. A specific revision can still be
            selected from a log view
    CHANGE: Only show the lead name when viewing directory contents
    
    FIX: Fixed error concerning use of pclose

---
+++ tags/1.20/filedetails.php
@@ -0,0 +1,77 @@
+<?php
+
+// WebSVN - Subversion repository viewing via the web using PHP
+// Copyright (C) 2004 Tim Armes
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --
+//
+// filedetails.php
+//
+// Simply lists the contents of a file
+
+require("include/config.inc");
+require("include/svnlook.inc");
+require("include/template.inc");
+
+$rep = @$_REQUEST["rep"];
+$path = @$_REQUEST["path"];
+$rev = @$_REQUEST["rev"];
+$showchanged = (@$_REQUEST["sc"] == 1)?1:0;
+
+// Make sure that we have a repository
+if (!isset($rep))
+{
+   echo $lang["NOREP"];
+   exit;
+}
+
+list ($repname, $reppath) = $config->getRepository($rep);
+$svnrep = new SVNRepository($reppath);
+
+if ($path{0} != "/")
+   $ppath = "/".$path;
+else
+   $ppath = $path;
+
+$passrev = $rev;
+
+// If there's no revision info, go to the lastest revision for this path
+$history = $svnrep->getHistory($path);
+$youngest = $history[0]["rev"];
+
+if (empty($rev))
+   $rev = $youngest;
+
+if ($rev != $youngest)
+   $vars["goyoungestlink"] = "<a href=\"filedetails.php?rep=$rep&path=$path&sc=1\">${lang["GOYOUNGEST"]}</a>";
+else
+   $vars["goyoungestlink"] = "";
+
+
+$vars["repname"] = $repname;
+$vars["rev"] = $rev;
+$vars["path"] = $ppath;
+$vars["prevdifflink"] = "<a href=\"diff.php?rep=$rep&path=$path&rev=$passrev&sc=$showchanged\">${lang["DIFFPREV"]}</a>";
+
+$listing = array ();
+
+$vars["version"] = $version;
+parseTemplate($config->templatePath."header.tmpl", $vars, $listing);
+parseTemplate($config->templatePath."file.tmpl", $vars, $listing);
+parseTemplate($config->templatePath."footer.tmpl", $vars, $listing);
+
+?>
