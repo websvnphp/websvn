cdfc93b687452be79936bf96beca3a5182555f33
spetters <spetters@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2007-09-04 15:51:44 +0000

    added uzbek translation (fixed #142) [trunk, branches/2.0] patch by Behzod Saidov

---
+++ languages/uzbek.php
@@ -0,0 +1,123 @@
+<?php
+
+// WebSVN - Subversion repository viewing via the web using PHP
+// Copyright (C) 2004 Tim Armes
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --
+//
+// uzbek.php
+//
+// Uzbek language strings
+// by Behzod Saidov <behzodsaidov@gmail.com>
+// UTF-8 encoding
+
+// The language name is displayed in the drop down box.  It MUST be encoded as Unicode (no HTML entities).
+$lang["LANGUAGENAME"] = "Uzbek";
+$lang['LANGUAGETAG'] = 'uz';
+
+$lang["LOG"] = "Log";
+$lang["DIFF"] = "Farq";
+
+$lang["NOREP"] = "Ombor ko'rsatilmagan";
+$lang["NOPATH"] = "Yo'l topilmadi";
+$lang["NOACCESS"] = "Bu katalogni o'qish uchun sizga ruxsat berilmagan";
+$lang["RESTRICTED"] = "Cheklangan ruxsat";
+$lang["SUPPLYREP"] = "Iltimos, repositoriylargacha bo'lgan yo'lni include/config.php faylida ko'rsating, bunda \$config->parentPath yoki \$config->addRepository<p> dan foydalanishingiz mumkin. Qo'shimcha ma'lumotlar o'rnatish bo'yicha qo'llanmada keltirilgan";
+
+$lang["DIFFREVS"] = "Tahrirlar orasidagi farq";
+$lang["AND"] = "va";
+$lang["REV"] = "Tahrir";
+$lang["LINE"] = "Satr";
+$lang["SHOWENTIREFILE"] = "faylni to'liq ko'rsatish";
+$lang["SHOWCOMPACT"] = "faqat farqli qismlarini ko'rsatish";
+
+$lang["DIFFPREV"] = "oldingisi bilan taqqoslash";
+$lang["BLAME"] = "mualliflik";
+
+$lang["REVINFO"] = "Tahrir haqida ma'limot";
+$lang["GOYOUNGEST"] = "eng yangi tahrirga o'tish";
+$lang["LASTMOD"] = "Oxirgi o'zgarish";
+$lang["LOGMSG"] = "Log yozuvi";
+$lang["CHANGES"] = "O'zgarishlar";
+$lang["SHOWCHANGED"] = "o'zgargan fayllarni ko'rsatish";
+$lang["HIDECHANGED"] = "o'zgargan fayllarni yashirish";
+$lang["NEWFILES"] = "Yangi fayllar";
+$lang["CHANGEDFILES"] = "O'zgargan fayllar";
+$lang["DELETEDFILES"] = "O'chirilgan fayllar";
+$lang["VIEWLOG"] = "loglarni&nbsp;ochish";
+$lang["PATH"] = "Yo'l";
+$lang["AUTHOR"] = "Muallif";
+$lang["AGE"] = "Yoshi";
+$lang["LOG"] = "Log";
+$lang["CURDIR"] = "Joriy katalog";
+$lang["TARBALL"] = "Arxiv";
+
+$lang["PREV"] = "old.";
+$lang["NEXT"] = "key.";
+$lang["SHOWALL"] = "hammasini ko'rsatish";
+
+$lang["BADCMD"] = "Buyruqni bajarishda xatolik ro'y berdi";
+$lang["UNKNOWNREVISION"] = "Tahrir topilmadi";
+
+$lang["POWERED"] = "<a href=\"http://websvn.tigris.org/\">WebSVN</a> dasturi ishlatilmoqda";
+$lang["PROJECTS"] = "Subversion ombori&nbsp;";
+$lang["SERVER"] = "Subversion serveri&nbsp;";
+
+$lang["FILTER"] = "Filtr parametrlari";
+$lang["STARTLOG"] = "Tahrirdan";
+$lang["ENDLOG"] = "Tahrirgacha";
+$lang["MAXLOG"] = "Maksimal tahrirlar";
+$lang["SEARCHLOG"] = "qidirish";
+$lang["CLEARLOG"] = "joriy filtrni tozalash";
+$lang["MORERESULTS"] = "yana qidirish...";
+$lang["NORESULTS"] = "So'rovingizga to'g'ri keladigan yozuv topilmadi";
+$lang["NOMORERESULTS"] = "So'rovingizga to'g'ri keladigan yozuvlar boshqa topilmadi";
+
+$lang["RSSFEEDTITLE"] = "WebSVN RSS tarmasi"; 
+$lang["FILESMODIFIED"] = "fayl(lar) o'zgartirilgan"; 
+$lang["RSSFEED"] = "RSS tasma"; 
+
+$lang["LINENO"] = "Satr raqami";
+$lang["BLAMEFOR"] = "Tahrir muallifi haqida ma'lumot ";
+
+$lang["DAYLETTER"] = "kun";
+$lang["HOURLETTER"] = "soat";
+$lang["MINUTELETTER"] = "daq.";
+$lang["SECONDLETTER"] = "son.";
+
+$lang["GO"] = "O'tish";
+
+$lang["PATHCOMPARISON"] = "Yo'llarni taqqoslash";
+$lang["COMPAREPATHS"] = "yo'llarni taqqoslash";
+$lang["COMPAREREVS"] = "Tahrirlarni taqqoslash";
+$lang["PROPCHANGES"] = "Xususiyatlardagi o'zgarishlar :";
+$lang["CONVFROM"] = "Bu taqqoslash yo'lni aylantirish uchun kerak bo'ladigan o'zgarishlarni ko'rsatadi. ";
+$lang["TO"] = "TO";
+$lang["REVCOMP"] = "teskari taqqosalash";
+$lang["COMPPATH"] = "Yo'lni taqqoslash:";
+$lang["WITHPATH"] = "Yo'l bilan:";
+$lang["FILEDELETED"] = "Fayl o'chirilgan";
+$lang["FILEADDED"] = "Yangi fayl";
+
+// The following are defined by some languages to stop unwanted line splitting
+// in the template files. 
+
+$lang["NOBR"] = "";
+$lang["ENDNOBR"] = "";
+
+//$lang["NOBR"] = "<nobr>";
+//$lang["ENDNOBR"] = "</nobr>";
