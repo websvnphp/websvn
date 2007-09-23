dfa64f0a45d5f483c100dc39b3c0a3872ece136b
spetters <spetters@users.noreply.github.com>
2007-09-23 18:00:55 +0000

    fixed uzbek translation (patch by Mashrab Kuvatov)

--- languages/uzbek.php
+++ languages/uzbek.php
@@ -32,34 +32,34 @@ $lang['LANGUAGETAG'] = 'uz';
 $lang["LOG"] = "Log";
 $lang["DIFF"] = "Farq";
 
-$lang["NOREP"] = "Ombor ko'rsatilmagan";
-$lang["NOPATH"] = "Yo'l topilmadi";
-$lang["NOACCESS"] = "Bu katalogni o'qish uchun sizga ruxsat berilmagan";
+$lang["NOREP"] = "Ombor ko?rsatilmagan";
+$lang["NOPATH"] = "Yo?l topilmadi";
+$lang["NOACCESS"] = "Bu katalogni o?qish uchun sizga ruxsat berilmagan";
 $lang["RESTRICTED"] = "Cheklangan ruxsat";
-$lang["SUPPLYREP"] = "Iltimos, repositoriylargacha bo'lgan yo'lni include/config.php faylida ko'rsating, bunda \$config->parentPath yoki \$config->addRepository<p> dan foydalanishingiz mumkin. Qo'shimcha ma'lumotlar o'rnatish bo'yicha qo'llanmada keltirilgan";
+$lang["SUPPLYREP"] = "Iltimos, repositoriylargacha bo?lgan yo?lni include/config.php faylida ko?rsating, bunda \$config->parentPath yoki \$config->addRepository<p> dan foydalanishingiz mumkin. Qo?shimcha ma'lumotlar o?rnatish bo?yicha qo?llanmada keltirilgan";
 
 $lang["DIFFREVS"] = "Tahrirlar orasidagi farq";
 $lang["AND"] = "va";
 $lang["REV"] = "Tahrir";
 $lang["LINE"] = "Satr";
-$lang["SHOWENTIREFILE"] = "faylni to'liq ko'rsatish";
-$lang["SHOWCOMPACT"] = "faqat farqli qismlarini ko'rsatish";
+$lang["SHOWENTIREFILE"] = "faylni to?liq ko?rsatish";
+$lang["SHOWCOMPACT"] = "faqat farqli qismlarini ko?rsatish";
 
 $lang["DIFFPREV"] = "oldingisi bilan taqqoslash";
 $lang["BLAME"] = "mualliflik";
 
 $lang["REVINFO"] = "Tahrir haqida ma'limot";
-$lang["GOYOUNGEST"] = "eng yangi tahrirga o'tish";
-$lang["LASTMOD"] = "Oxirgi o'zgarish";
+$lang["GOYOUNGEST"] = "eng yangi tahrirga o?tish";
+$lang["LASTMOD"] = "Oxirgi o?zgarish";
 $lang["LOGMSG"] = "Log yozuvi";
-$lang["CHANGES"] = "O'zgarishlar";
-$lang["SHOWCHANGED"] = "o'zgargan fayllarni ko'rsatish";
-$lang["HIDECHANGED"] = "o'zgargan fayllarni yashirish";
+$lang["CHANGES"] = "O?zgarishlar";
+$lang["SHOWCHANGED"] = "o?zgargan fayllarni ko?rsatish";
+$lang["HIDECHANGED"] = "o?zgargan fayllarni yashirish";
 $lang["NEWFILES"] = "Yangi fayllar";
-$lang["CHANGEDFILES"] = "O'zgargan fayllar";
-$lang["DELETEDFILES"] = "O'chirilgan fayllar";
+$lang["CHANGEDFILES"] = "O?zgargan fayllar";
+$lang["DELETEDFILES"] = "O?chirilgan fayllar";
 $lang["VIEWLOG"] = "loglarni&nbsp;ochish";
-$lang["PATH"] = "Yo'l";
+$lang["PATH"] = "Yo?l";
 $lang["AUTHOR"] = "Muallif";
 $lang["AGE"] = "Yoshi";
 $lang["LOG"] = "Log";
@@ -68,9 +68,9 @@ $lang["TARBALL"] = "Arxiv";
 
 $lang["PREV"] = "old.";
 $lang["NEXT"] = "key.";
-$lang["SHOWALL"] = "hammasini ko'rsatish";
+$lang["SHOWALL"] = "hammasini ko?rsatish";
 
-$lang["BADCMD"] = "Buyruqni bajarishda xatolik ro'y berdi";
+$lang["BADCMD"] = "Buyruqni bajarishda xatolik ro?y berdi";
 $lang["UNKNOWNREVISION"] = "Tahrir topilmadi";
 
 $lang["POWERED"] = "<a href=\"http://websvn.tigris.org/\">WebSVN</a> dasturi ishlatilmoqda";
@@ -84,33 +84,33 @@ $lang["MAXLOG"] = "Maksimal tahrirlar";
 $lang["SEARCHLOG"] = "qidirish";
 $lang["CLEARLOG"] = "joriy filtrni tozalash";
 $lang["MORERESULTS"] = "yana qidirish...";
-$lang["NORESULTS"] = "So'rovingizga to'g'ri keladigan yozuv topilmadi";
-$lang["NOMORERESULTS"] = "So'rovingizga to'g'ri keladigan yozuvlar boshqa topilmadi";
+$lang["NORESULTS"] = "So?rovingizga to?g?ri keladigan yozuv topilmadi";
+$lang["NOMORERESULTS"] = "So?rovingizga to?g?ri keladigan yozuvlar boshqa topilmadi";
 
 $lang["RSSFEEDTITLE"] = "WebSVN RSS tarmasi"; 
-$lang["FILESMODIFIED"] = "fayl(lar) o'zgartirilgan"; 
+$lang["FILESMODIFIED"] = "fayl(lar) o?zgartirilgan"; 
 $lang["RSSFEED"] = "RSS tasma"; 
 
 $lang["LINENO"] = "Satr raqami";
-$lang["BLAMEFOR"] = "Tahrir muallifi haqida ma'lumot ";
+$lang["BLAMEFOR"] = "Tahrir muallifi haqida ma'lumot";
 
 $lang["DAYLETTER"] = "kun";
 $lang["HOURLETTER"] = "soat";
 $lang["MINUTELETTER"] = "daq.";
 $lang["SECONDLETTER"] = "son.";
 
-$lang["GO"] = "O'tish";
+$lang["GO"] = "O?tish";
 
-$lang["PATHCOMPARISON"] = "Yo'llarni taqqoslash";
-$lang["COMPAREPATHS"] = "yo'llarni taqqoslash";
+$lang["PATHCOMPARISON"] = "Yo?llarni taqqoslash";
+$lang["COMPAREPATHS"] = "yo?llarni taqqoslash";
 $lang["COMPAREREVS"] = "Tahrirlarni taqqoslash";
-$lang["PROPCHANGES"] = "Xususiyatlardagi o'zgarishlar :";
-$lang["CONVFROM"] = "Bu taqqoslash yo'lni aylantirish uchun kerak bo'ladigan o'zgarishlarni ko'rsatadi. ";
+$lang["PROPCHANGES"] = "Xususiyatlardagi o?zgarishlar :";
+$lang["CONVFROM"] = "Bu taqqoslash yo?lni aylantirish uchun kerak bo?ladigan o?zgarishlarni ko?rsatadi.";
 $lang["TO"] = "TO";
-$lang["REVCOMP"] = "teskari taqqosalash";
-$lang["COMPPATH"] = "Yo'lni taqqoslash:";
-$lang["WITHPATH"] = "Yo'l bilan:";
-$lang["FILEDELETED"] = "Fayl o'chirilgan";
+$lang["REVCOMP"] = "teskari taqqoslash";
+$lang["COMPPATH"] = "Yo?lni taqqoslash:";
+$lang["WITHPATH"] = "Yo?l bilan:";
+$lang["FILEDELETED"] = "Fayl o?chirilgan";
 $lang["FILEADDED"] = "Yangi fayl";
 
 // The following are defined by some languages to stop unwanted line splitting
