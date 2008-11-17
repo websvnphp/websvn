7a131178d6424793c0cbaf4dffa0ec863b5b4a55
Dirk Thomas <dthomas@osrfoundation.org>
2008-11-17 08:51:34 +0000

    updated geshi to 1.0.8.1

--- lib/geshi/mysql.php
+++ lib/geshi/mysql.php
@@ -1,10 +1,10 @@
 <?php
- /*************************************************************************************
+/*************************************************************************************
  * mysql.php
  * ---------
  * Author: Carl F�rstenberg (azatoth@gmail.com)
  * Copyright: (c) 2005 Carl F�rstenberg, Nigel McNie (http://qbnz.com/highlighter)
- * Release Version: 1\.0\.8
+ * Release Version: 1.0.8.1
  * Date Started: 2004/06/04
  *
  * MySQL language file for GeSHi.
@@ -34,97 +34,115 @@ $language_data = array (
     'LANG_NAME' => 'MySQL',
     'COMMENT_SINGLE' => array(1 =>'--', 2 => '#'),
     'COMMENT_MULTI' => array('/*' => '*/'),
-    'CASE_KEYWORDS' => 1,
-    'QUOTEMARKS' => array("'", '"'),
+    'CASE_KEYWORDS' => GESHI_CAPS_UPPER,
+    'QUOTEMARKS' => array("'", '"', '`'),
     'ESCAPE_CHAR' => '\\',
+    'NUMBERS' =>
+        GESHI_NUMBER_INT_BASIC |
+        GESHI_NUMBER_OCT_PREFIX |
+        GESHI_NUMBER_HEX_PREFIX |
+        GESHI_NUMBER_FLT_NONSCI |
+        GESHI_NUMBER_FLT_SCI_SHORT |
+        GESHI_NUMBER_FLT_SCI_ZERO,
     'KEYWORDS' => array(
         1 => array(
             /* Mix */
-            'ALTER DATABASE', 'ALTER TABLE', 'CREATE DATABASE', 'CREATE INDEX', 'CREATE TABLE', 'DROP DATABASE',
-            'DROP INDEX', 'DROP TABLE', 'RENAME TABLE', 'DELETE', 'DO', 'HANDLER', 'INSERT', 'LOAD DATA INFILE',
-            'REPLACE', 'SELECT', 'TRUNCATE', 'UPDATE', 'DESCRIBE', 'USE', 'START TRANSACTION', 'COMMIT', 'ROLLBACK',
-            'SAVEPOINT', 'ROLLBACK TO SAVEPOINT', 'LOCK TABLES', 'UNLOCK_TABLES', 'SET TRANACTIONS', 'SET', 'SHOW',
-            'CREATE PROCEDURE', 'CREATE FUNCTION', 'ALTER PROCEDURE', 'ALTER FUNCTION', 'DROP PROCEDURE', 'DROP FUNCTION',
-            'SHOW CREATE PROCEDURE', 'SHOW CREATE FUNCTION', 'SHOW PROCEDURE STATUS', 'SHOW FUNCTION STATUS',
-            'CALL', 'BEGIN', 'END', 'DECLARE', 'CREATE ROUTINE', 'ALTER ROUTINE', 'CREATE', 'ALTER', 'DROP',
-            'PRIMARY KEY', 'VALUES', 'INTO', 'FROM',
-            'ANALYZE', 'BDB', 'BERKELEYDB', 'BTREE', 'BY', 'CASCADE', 'CHECK', 'COLUMN', 'COLUMNS', 'CONSTRAINT',
-            'CROSS', 'DATABASES', 'DELAYED', 'DISTINCT', 'DISTINCTROW', 'ENCLOSED', 'ERRORS', 'ESCAPED', 'EXISTS',
-            'EXPLAIN', 'FALSE', 'FIELDS', 'FORCE', 'FOREIGN', 'FULLTEXT', 'GEOMETRY', 'GRANT', 'GROUP', 'HASH',
-            'HAVING', 'HELP', 'HIGH_PRIORITY', 'IGNORE', 'INNER', 'INNODB', 'INTERVAL', 'JOIN', 'KEYS', 'KILL',
-            'LINES', 'LOW_PRIORITY', 'MASTER_SERVER_ID', 'MATCH', 'MIDDLEINT', 'MRG_MYISAM', 'NATURAL', 'OPTIMIZE',
-            'OPTION', 'OPTIONALLY', 'ORDER', 'OUTER', 'OUTFILE', 'PRIVILEGES', 'PURGE', 'READ', 'REFERENCES',
-            'REQUIRE', 'RESTRICT', 'RETURNS', 'REVOKE', 'RLIKE', 'RTREE', 'SOME', 'SONAME', 'SPATIAL', 'SQL_BIG_RESULT',
-            'SQL_CALC_FOUND_ROWS', 'SQL_SMALL_RESULT', 'SSL', 'STARTING', 'STRAIGHT_JOIN', 'STRIPED', 'TERMINATED',
-            'TRUE', 'TYPES', 'UNION', 'USAGE', 'USER_RESOURCES', 'USING', 'VARCHARACTER', 'WARNINGS', 'WHERE', 'WRITE',
+            'ACTION','ADD','ALTER','ANALYZE','ASC','BDB','BEGIN','BERKELEYDB',
+            'BTREE','BY','CALL','CASCADE','CHECK','COLUMN','COLUMNS','COMMIT',
+            'CONSTRAINT','CREATE','CROSS','DATA','DATABASE','DATABASES',
+            'DECLARE','DELAYED','DELETE','DESC','DESCRIBE','DISTINCT',
+            'DISTINCTROW','DO','DROP','ENCLOSED','END','ENGINE','ELSE','ERRORS',
+            'ESCAPED','EXISTS','EXPLAIN','FALSE','FIELDS','FORCE','FOREIGN',
+            'FROM','FULLTEXT','FUNCTION','GEOMETRY','GRANT','GROUP','HANDLER',
+            'HASH','HAVING','HELP','HIGH_PRIORITY','IGNORE','INDEX','INFILE',
+            'INNER','INNODB','INSERT','INTERVAL','INTO','JOIN','KEY','KEYS',
+            'KILL','LIMIT','LINES','LOAD','LOCK','LOW_PRIORITY',
+            'MASTER_SERVER_ID','MATCH','MIDDLEINT','MODIFY','MRG_MYISAM',
+            'NATURAL','NO','ON','OPTIMIZE','OPTION','OPTIONALLY','ORDER',
+            'OUTER','OUTFILE','PRIMARY','PRIVILEGES','PROCEDURE','PURGE','READ',
+            'REFERENCES','RENAME','REPLACE','REQUIRE','RESTRICT','RETURNS',
+            'REVOKE','RLIKE','ROLLBACK','ROUTINE','RTREE','SAVEPOINT','SCHEMA',
+            'SELECT','SET','SHOW','SOME','SONAME','SPATIAL','SQL_BIG_RESULT',
+            'SQL_CALC_FOUND_ROWS','SQL_SMALL_RESULT','SSL','START','STARTING',
+            'STATUS','STRAIGHT_JOIN','STRIPED','TABLE','TABLES','TERMINATED',
+            'TO','TRANSACTIONS','TRANSACTION','TRUE','TRUNCATE','TYPES','UNION',
+            'UNLOCK_TABLES','UPDATE','USAGE','USE','USER_RESOURCES','USING',
+            'VALUES','VARCHARACTER','WARNINGS','WHEN','WHERE','WRITE',
 
             /* Control Flow Functions */
-            'CASE', 'WHEN', 'THEN', 'ELSE', 'END',
-
-            /* String Functions */
-            'UNHEX', 'BIN', 'BIT_LENGTH', 'CHAR_LENGTH', 'CHARACTER_LENGTH', 'COMPRESS', 'CONCAT',
-            'CONCAT_WS', 'CONV', 'ELT', 'EXPORT_SET', 'FIELD', 'FIND_IN_SET', 'FORMAT', 'HEX',
-            'INSERT', 'INSTR', 'LCASE', 'LEFT', 'LENGTH', 'LOAD_FILE', 'LOCATE', 'LOWER', 'LPAD',
-            'LTRIM', 'MAKE_SET', 'MID', 'OCT', 'OCTET_LENGTH', 'ORD', 'POSITION', 'QUOTE', 'REPEAT',
-            'REPLACE', 'REVERSE', 'RIGHT', 'RPAD', 'RTRIM', 'SOUNDEX', 'SPACE', 'SUBSTRING',
-            'SUBSTRING_INDEX', 'TRIM', 'UCASE', 'UPPER', 'UNCOMPRESS', 'UNCOMPRESSD_LENGTH',
-            'MD5', 'SHA1',
-
-            /* Numeric Functions */
-            'ABS', 'ACOS', 'ASIN', 'ATAN', 'ATAN2', 'CEILING', 'CEIL', 'COS', 'COT', 'CRC32', 'DEGREES',
-            'EXP', 'FLOOR', 'LN', 'LOG', 'LOG2', 'LOG10', 'MOD', 'PI', 'POW', 'POWER', 'RADIANS', 'RAND',
-            'ROUND', 'SIGN', 'SIN', 'SQRT', 'TAN', 'TRUNCATE',
-
-            /* Date and Time Functions */
-            'ADDDATE', 'ADDTIME', 'CONVERT_TZ', 'CURDATE', 'CURRENT_DATE', 'CURTIME', 'CURRENT_TIME',
-            'CURRENT_TIMESTAMP', 'DATEDIFF', 'DATE_ADD', 'DATE_SUB', 'DATE_FORMAT', 'DAY',
-            'DAYNAME', 'DAYOFMONTH', 'DAYOFWEEK', 'DAYOFYEAR', 'EXTRACT', 'FROM_DAYS', 'FROM_UNIXTIME',
-            'GET_FORMAT', 'LAST_DAY', 'LOCALTIME', 'LOCALTIMESTAMP', 'MAKEDATE', 'MAKETIME',
-            'MICROSECOND', 'MONTHNAME', 'NOW', 'PERIOD_ADD', 'PERIOD_DIFF', 'QUARTER',
-            'SECOND', 'SEC_TO_TIME', 'STR_TO_DATE', 'SUBDATE', 'SUBTIME', 'SYSDATE', 'TIME', 'TIMEDIFF',
-            'TIMESTAMP', 'TIMESTAMPADD', 'TIMESTAMPDIFF', 'TIME_FORMAT', 'TIME_TO_SEC', 'TO_DAYS',
-            'UNIX_TIMESTAMP', 'UTC_DATE', 'UTC_TIME', 'UTC_TIMESTAMP', 'WEEKDAY', 'WEEKOFYEAR',
-            'YEARWEEK',
+            'CASE','IF','THEN',
             ),
         2 => array(
-            'INTEGER', 'SMALLINT', 'DECIMAL', 'NUMERIC', 'FLOAT', 'REAL', 'DOUBLE PRECISION',
-            'DOUBLE', 'INT', 'DEC', 'BIT' ,'TINYINT', 'SMALLINT', 'MEDIUMINT', 'BIGINT',
-            'DATETIME', 'DATE', 'TIMESTAMP', 'TIME', 'YEAR',
-            'CHAR', 'VARCHAR', 'BINARY', 'CHARACTER VARYING', 'VARBINARY', 'TINYBLOB', 'TINYTEXT',
-            'BLOB', 'TEXT','MEDIUMBLOB', 'MEDIUMTEXT', 'LONGBLOB', 'LONGTEXT', 'ENUM', 'SET',
-            'SERIAL DEFAULT VALUE', 'SERIAL', 'FIXED'
+            'BIGINT','BINARY','BIT','BLOB','BOOLEAN','CHAR','CHARACTER VARYING',
+            'DATE','DATETIME','DEC','DECIMAL','DOUBLE','ENUM','FIXED','FLOAT',
+            'INT','INTEGER','LONGBLOB','LONGTEXT','MEDIUMBLOB','MEDIUMINT',
+            'MEDIUMTEXT','NUMERIC','PRECISION','REAL','SERIAL DEFAULT VALUE',
+            'SERIAL','SET','SMALLINT','TEXT','TIME','TIMESTAMP','TINYBLOB',
+            'TINYINT','TINYTEXT','VARBINARY','VARCHAR'
             ),
         3 => array(
-            'ZEROFILL', 'NOT NULL', 'UNSIGNED', 'AUTO_INCREMENT', 'UNIQUE', 'NOT', 'NULL', 'CHARACTER SET', 'CHARSET',
-            'ASCII', 'UNICODE', 'NATIONAL', 'BOTH', 'LEADING', 'TRAILING','DEFAULT'
+            'ASCII','AUTO_INCREMENT','BOTH','CHARACTER','CHARSET','DEFAULT',
+            'LEADING','NATIONAL','NULL','TRAILING','UNICODE','UNIQUE',
+            'UNSIGNED','ZEROFILL'
             ),
         4 => array(
-            'MICROSECOND', 'SECOND', 'MINUTE', 'HOUR', 'DAY', 'WEEK', 'MONTH', 'QUARTER', 'YEAR', 'SECOND_MICROSECOND',
-            'MINUTE_MICROSECOND', 'MINUTE_SECOND', 'HOUR_MICROSECOND', 'HOUR_SECOND', 'HOUR_MINUTE', 'DAY_MICROSECOND',
-            'DAY_SECOND', 'DAY_MINUTE', 'DAY_HOUR', 'YEAR_MONTH'
+            'DAY','DAY_HOUR','DAY_MICROSECOND','DAY_MINUTE','DAY_SECOND','HOUR',
+            'HOUR_MICROSECOND','HOUR_MINUTE','HOUR_SECOND','MICROSECOND',
+            'MINUTE','MINUTE_MICROSECOND','MINUTE_SECOND','MONTH','QUARTER',
+            'SECOND','SECOND_MICROSECOND','WEEK','YEAR','YEAR_MONTH'
             ),
         5 => array(
-            'OR', 'XOR', 'AND', 'NOT', 'BETWEEN', 'IS', 'LIKE', 'REGEXP', 'IN', 'DIV',
-            'MOD', 'BINARY', 'COLLATE', 'LIMIT', 'OFFSET'
+            'AND','BETWEEN','BINARY','COLLATE','DIV','IN','IS','LIKE',
+            'MOD','NOT','OFFSET','OR','REGEXP','XOR'
+            ),
+        6 => array(
+            /* String Functions */
+            'BIN','BIT_LENGTH','CHAR_LENGTH','CHARACTER_LENGTH','COMPRESS',
+            'CONCAT','CONCAT_WS','CONV','ELT','EXPORT_SET','FIELD',
+            'FIND_IN_SET','FORMAT','HEX','INSTR','LCASE','LEFT','LENGTH',
+            'LOAD_FILE','LOCATE','LOWER','LPAD','LTRIM','MAKE_SET','MD5','MID',
+            'OCT','OCTET_LENGTH','ORD','POSITION','QUOTE','REPEAT','REVERSE',
+            'RIGHT','RPAD','RTRIM','SHA1','SOUNDEX','SPACE','SUBSTRING',
+            'SUBSTRING_INDEX','TRIM','UCASE','UNCOMPRESS','UNCOMPRESSD_LENGTH',
+            'UNHEX','UPPER',
+            ),
+        7 => array(
+            /* Numeric Functions */
+            'ABS','ACOS','ASIN','ATAN','ATAN2','AVG','CEILING','CEIL','COS',
+            'COT','CRC32','DEGREES','EXP','FLOOR','LN','LOG','LOG2','LOG10',
+            'MIN','MAX','PI','POW','POWER','RADIANS','RAND','ROUND','SIGN',
+            'SIN','SQRT','SUM','TAN',
+            ),
+        8 => array(
+            /* Date and Time Functions */
+            'ADDDATE','ADDTIME','CONVERT_TZ','CURDATE','CURRENT_DATE',
+            'CURRENT_TIME','CURRENT_TIMESTAMP','CURTIME','DATE_ADD',
+            'DATE_FORMAT','DATE_SUB','DATEDIFF','DAY','DAYNAME','DAYOFMONTH',
+            'DAYOFWEEK','DAYOFYEAR','EXTRACT','FROM_DAYS','FROM_UNIXTIME',
+            'GET_FORMAT','LAST_DAY','LOCALTIME','LOCALTIMESTAMP','MAKEDATE',
+            'MAKETIME','MICROSECOND','MONTHNAME','NOW','PERIOD_ADD',
+            'PERIOD_DIFF','QUARTER','SEC_TO_TIME','SECOND','STR_TO_DATE',
+            'SUBDATE','SUBTIME','SYSDATE','TIME','TIME_FORMAT','TIME_TO_SEC',
+            'TIMEDIFF','TIMESTAMP','TIMESTAMPADD','TIMESTAMPDIFF','TO_DAYS',
+            'UNIX_TIMESTAMP','UTC_DATE','UTC_TIME','UTC_TIMESTAMP','WEEKDAY',
+            'WEEKOFYEAR','YEARWEEK',
             ),
         ),
     'SYMBOLS' => array(
         ':=',
-        '||', 'OR', 'XOR',
-        '&&', 'AND',
-        'NOT',
-        'BETWEEN', 'CASE', 'WHEN', 'THEN', 'ELSE',
-        '=', '<=>', '>=', '>', '<=', '<', '<>', '!=', 'IS', 'LIKE', 'REGEXP', 'IN',
+        '||',
+        '&&',
+        '=', '<=>', '>=', '>', '<=', '<', '<>', '!=',
         '|',
         '&',
         '<<', '>>',
         '-', '+',
-        '*', '/', 'DIV', '%', 'MOD',
+        '*', '/', '%',
         '^',
         '~',
         '!',
-        'BINARY', 'COLLATE',
         '(', ')',
+        ',', ';'
         ),
     'CASE_SENSITIVE' => array(
         GESHI_COMMENTS => false,
@@ -132,37 +150,43 @@ $language_data = array (
         2 => false,
         3 => false,
         4 => false,
-        5 => false
+        5 => false,
+        6 => false,
+        7 => false,
+        8 => false
         ),
     'STYLES' => array(
         'KEYWORDS' => array(
-            1 => 'color: #993333; font-weight: bold;',
-            2 => 'color: #aa9933; font-weight: bold;',
-            3 => 'color: #aa3399; font-weight: bold;',
-            4 => 'color: #33aa99; font-weight: bold;',
-            5 => 'color: #993333; font-weight: bold;'
+            1 => 'color: #990099; font-weight: bold;',
+            2 => 'color: #FF9900; font-weight: bold;',
+            3 => 'color: #9900FF; font-weight: bold;',
+            4 => 'color: #990099; font-weight: bold;',
+            5 => 'color: #CC0099; font-weight: bold;',
+            6 => 'color: #000099; font-weight: bold;',
+            7 => 'color: #000099; font-weight: bold;',
+            8 => 'color: #000099; font-weight: bold;'
             ),
         'COMMENTS' => array(
-            'MULTI' => 'color: #808080; font-style: italic;',
-            1 => 'color: #808080; font-style: italic;',
-            2 => 'color: #808080; font-style: italic;'
+            'MULTI' => 'color: #808000; font-style: italic;',
+            1 => 'color: #808000; font-style: italic;',
+            2 => 'color: #808000; font-style: italic;'
             ),
         'ESCAPE_CHAR' => array(
-            0 => 'color: #000099; font-weight: bold;'
+            0 => 'color: #004000; font-weight: bold;'
             ),
         'BRACKETS' => array(
-            0 => 'color: #66cc66;'
+            0 => 'color: #FF00FF;'
             ),
         'STRINGS' => array(
-            0 => 'color: #ff0000;'
+            0 => 'color: #008000;'
             ),
         'NUMBERS' => array(
-            0 => 'color: #cc66cc;'
+            0 => 'color: #008080;'
             ),
         'METHODS' => array(
             ),
         'SYMBOLS' => array(
-            0 => 'color: #66cc66;'
+            0 => 'color: #FF00FF;'
             ),
         'SCRIPT' => array(
             ),
@@ -174,7 +198,10 @@ $language_data = array (
         2 => '',
         3 => '',
         4 => '',
-        5 => ''
+        5 => '',
+        6 => '',
+        7 => '',
+        8 => ''
         ),
     'OOLANG' => false,
     'OBJECT_SPLITTERS' => array(
