604519bbae1a326abe2acae624a64f77c251581b
dirkthomas <dirkthomas@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2009-01-22 10:54:24 +0000

    added pear Archive_Tar lib to create tar archives (without requiring command line program) (patch provided by Damien Martin-Guillerez) added usage of gz extension if available (to not require command line program) fixed creating identical archives for the same requests

--- dl.php
+++ dl.php
@@ -26,6 +26,31 @@ require_once("include/setup.php");
 require_once("include/svnlook.php");
 require_once("include/utils.php");
 
+ini_set('include_path', $locwebsvnreal.'/lib/pear'.$config->pathSeparator.ini_get('include_path'));
+@include_once("Archive/Tar.php");
+
+function setDirectoryTimestamp($dir, $ts) {
+  global $config;
+  // changing the modification time of a directory under windows is only supported since php 5.3.0
+  if (!$config->serverIsWindows || version_compare(PHP_VERSION, '5.3.0alpha') !== -1) {
+    touch($dir, $ts);
+
+    $handle = opendir($dir);
+    if ($handle) {
+      while (($file = readdir($handle)) !== false) {
+        if ($file == '.' || $file == '..') {
+          continue;
+        }
+        $f = $dir.DIRECTORY_SEPARATOR.$file;
+        if (is_dir($f)) {
+          setDirectoryTimestamp($f, $ts);
+        }
+      }
+      closedir($handle);
+    }
+  }
+}
+
 function removeDirectory($dir) {
   if (is_dir($dir)) {
     $dir = rtrim($dir, '/');
@@ -92,20 +117,30 @@ if (mkdir($tmpname)) {
 
   $svnrep->exportDirectory($path, $tmpname.DIRECTORY_SEPARATOR.$arcname, $rev);
 
+  // Set datetime of exported directory (and subdirectories) to datetime of revision so that every archive is equal
+  $date = $logEntry->date;
+  $ts = mktime(substr($date, 11, 2), substr($date, 14, 2), substr($date, 17, 2), substr($date, 5, 2), substr($date, 8, 2), substr($date, 0, 4));
+  setDirectoryTimestamp($tmpname.DIRECTORY_SEPARATOR.$arcname, $ts);
+
   // change to temp directory so that only relative paths are stored in tar
   chdir($tmpname);
 
-  // Set datetime of exported and directory to datetime of revision
-  $date = $logEntry->date;
-  $ts = mktime(substr($date, 11, 2), substr($date, 14, 2), substr($date, 17, 2), substr($date, 5, 2), substr($date, 8, 2), substr($date, 0, 4));
-  touch($arcname, $ts);
-
   // Create the tar file
   $retcode = 0;
-  $cmd = $config->tar.' -cf '.quote($tararc).' '.quote($arcname);
-  execCommand($cmd, $retcode);
-  if ($retcode != 0) {
-    print'Unable to call tar command "'.$config->tar.'"';
+  if (class_exists('Archive_Tar')) {
+    $tar = new Archive_Tar($tararc);
+    $created = $tar->create($arcname);
+    if (!$created) {
+      $retcode = 1;
+      print'Unable to create tar archive';
+    }
+
+  } else {
+    $cmd = $config->tar.' -cf '.quote($tararc).' '.quote($arcname);
+    execCommand($cmd, $retcode);
+    if ($retcode != 0) {
+      print'Unable to call tar command "'.$config->tar.'"';
+    }
   }
   if ($retcode != 0) {
     chdir('..');
@@ -117,14 +152,31 @@ if (mkdir($tmpname)) {
   touch($tararc, $ts);
 
   // ZIP it up
-  $cmd = $config->gzip.' '.quote($tararc);
-  $retcode = 0;
-  execCommand($cmd, $retcode);
-  if ($retcode != 0) {
-    print'Unable to call gzip command "'.$config->gzip.'"';
-    chdir('..');
-    removeDirectory($tmpname);
-    exit(0);
+  if (function_exists('gzopen')) {
+    $srcHandle = fopen($tmpname.DIRECTORY_SEPARATOR.$tararc, 'rb');
+    $dstHandle = gzopen($tmpname.DIRECTORY_SEPARATOR.$gzarc, 'wb');
+    if (!$srcHandle || !$dstHandle) {
+      print'Unable to open file for gz-compression';
+      chdir('..');
+      removeDirectory($tmpname);
+      exit(0);
+    }
+    while (!feof($srcHandle)) {
+      gzwrite($dstHandle, fread($srcHandle, 1024 * 512));
+    }
+    fclose($srcHandle);
+    gzclose($dstHandle);
+
+  } else {
+    $cmd = $config->gzip.' '.quote($tararc);
+    $retcode = 0;
+    execCommand($cmd, $retcode);
+    if ($retcode != 0) {
+      print'Unable to call gzip command "'.$config->gzip.'"';
+      chdir('..');
+      removeDirectory($tmpname);
+      exit(0);
+    }
   }
 
   // Give the file to the browser
