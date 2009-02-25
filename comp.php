d1c8c44ea42859a4c003ab25a56485f0c8967325
Dirk Thomas <dthomas@osrfoundation.org>
2009-02-25 16:27:10 +0000

    fixed compare path form in compare view

--- comp.php
+++ comp.php
@@ -59,12 +59,12 @@ $ignoreWhitespace = (@$_REQUEST["ignorews"] == 1);
 // Some page links put the revision with the path...
 if (strpos($path1, "@")) list($path1, $rev1) = explode("@", $path1);
 // Something went wrong. The path is missing.
-else if (strpos($path1, "@") == 0) {
+else if (strpos($path1, "@") === 0) {
   $rev1 = substr($path1, 1);
   $path1 = '/';
 }
 if (strpos($path2, "@")) list($path2, $rev2) = explode("@", $path2);
-else if (strpos($path2, "@") == 0) {
+else if (strpos($path2, "@") === 0) {
   $rev2 = substr($path2, 1);
   $path2 = '/';
 }
