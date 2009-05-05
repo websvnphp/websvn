44c7cc9102f65254a7bda85364f38ee57562f681
quinntaylor <quinntaylor@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2009-05-05 00:15:34 +0000

    Changed variable "date" to "revdate" in log.php, fixed in templates and docs. (Fixed #236)

--- log.php
+++ log.php
@@ -260,7 +260,7 @@ if (!empty($history)) {
       }
 
       $listing[$index]["revauthor"] = $r->author;
-      $listing[$index]["date"] = $r->date;
+      $listing[$index]["revdate"] = $r->date;
       $listing[$index]["revage"] = $r->age;
       $listing[$index]["revlog"] = nl2br($bugtraq->replaceIDs(create_anchors($r->msg)));
       $listing[$index]["rowparity"] = $row;
