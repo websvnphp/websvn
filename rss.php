4a0ab1bdbe9dfc88d0981526983f29ee58b25b8e
dirkthomas <dirkthomas@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2009-10-12 16:29:20 +0000

    improved handling of invalid requests in rss

--- rss.php
+++ rss.php
@@ -51,8 +51,15 @@ if ($path == '' || $path{0} != '/') {
   $ppath = $path;
 }
 
+if (!$rep) {
+  header('HTTP/1.x 404 Not Found', true, 404);
+  print 'Unable to access resource at path:  '.$path;
+  exit;
+}
 // Make sure that the user has full access to the specified directory
 if (!$rep->hasReadAccess($path, false)) {
+  header('HTTP/1.x 403 Forbidden', true, 403);
+  print 'Unable to access resource at path:  '.$path;
   exit;
 }
 
