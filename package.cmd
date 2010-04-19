1ffb032687b5c083ee5ff335798e641df674326f
dirkthomas <dirkthomas@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2010-04-19 16:25:06 +0000

    added doc for release process added scripts to do the release packaging

---
+++ package.cmd
@@ -0,0 +1,4 @@
+@echo off
+SET VERSION="2.3.1"
+svn export --no-auth-cache --username guest --password "" http://websvn.tigris.org/svn/websvn/tags/%VERSION% websvn-%VERSION%
+"C:\Program Files\7-Zip\7z.exe" a -r websvn-%VERSION%.zip websvn-%VERSION%/*
---
+++ package.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+VERSION="2.3.1"
+svn export --no-auth-cache --username guest --password "" http://websvn.tigris.org/svn/websvn/tags/$VERSION websvn-$VERSION
+tar -cf websvn-$VERSION.tar websvn-$VERSION
+gzip -9 websvn-$VERSION.tar
