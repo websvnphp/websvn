de0e349e6cba16e610ab00f04ae2c3e43106e47a
dirkthomas <dirkthomas@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2010-10-28 08:25:52 +0000

    fixed links in modification view for subpath repositories

--- trunk/include/svnlook.php
+++ trunk/include/svnlook.php
@@ -1010,6 +1010,16 @@ class SVNRepository {
 					unset($curLog->entries[$entryKey]->mods[$modKey]);
 					$fullModAccess = false;
 				}
+
+				// fix paths if command was for a subpath repository
+				if ($this->repConfig->subpath !== null) {
+					if (substr($mod->path, 0, strlen($this->repConfig->subpath) + 1) === '/'. $this->repConfig->subpath) {
+						$curLog->entries[$entryKey]->mods[$modKey]->path = substr($mod->path, strlen($this->repConfig->subpath) + 1);
+					} else {
+						$vars['error'] = 'Log entries do not start with subpath for repository with subpath';
+						return null;
+					}
+				}
 			}
 			if (!$fullModAccess) {
 				// hide commit message when access to any of the entries is prohibited
