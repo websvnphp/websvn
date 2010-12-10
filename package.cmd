c3428524399e1014978294d6b2f5c9961662753b
dirkthomas <dirkthomas@fe7299c5-eb4f-f749-b699-e8aa01759b72>
2010-12-10 13:48:10 +0000

    updated packaging scripts to latest release

--- package.cmd
+++ package.cmd
@@ -1,4 +1,4 @@
 @echo off
-SET VERSION="2.3.1"
+SET VERSION="2.3.2"
 svn export --no-auth-cache --username guest --password "" http://websvn.tigris.org/svn/websvn/tags/%VERSION% websvn-%VERSION%
 "C:\Program Files\7-Zip\7z.exe" a -r websvn-%VERSION%.zip websvn-%VERSION%/*
