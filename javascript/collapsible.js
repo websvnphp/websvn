5c40adf8973b892e54fd0cad3a0651b62f73f89d
k10blogger <k10blogger@gmail.com>
2020-09-09 09:03:16 +0530

    Improved Readability. By newline addition. Variables changed to camel case.

--- javascript/collapsible.js
+++ javascript/collapsible.js
@@ -1,90 +1,107 @@
 var tableRows = document.getElementsByTagName('tr');
 
-function toggleGroup(groupName) {
-  for (var i = 0; i < tableRows.length; i++) {
-    if (tableRows[i].title == groupName) {
-      if (tableRows[i].style.display == 'none') {
-        tableRows[i].style.display = 'table-row';
-      } else {
-        tableRows[i].style.display = 'none';
-      }
+function toggleGroup(groupName) 
+{
+
+    for (var i = 0; i < tableRows.length; i++) 
+    {
+        if (tableRows[i].title == groupName) 
+        {
+            if (tableRows[i].style.display == 'none') 
+            {
+                tableRows[i].style.display = 'table-row';
+            }
+            else 
+            {
+                tableRows[i].style.display = 'none';
+            }
+        }
     }
-  }
+
 }
 
-function collapseAllGroups() {
-  for (var i = 0; i < tableRows.length; i++) {
-    if (tableRows[i].title != '')
-      tableRows[i].style.display = 'none';
-  }
+function collapseAllGroups() 
+{
+
+    for (var i = 0; i < tableRows.length; i++) 
+    {
+        if (tableRows[i].title != '')
+            tableRows[i].style.display = 'none';
+    }
+
 }
 
-var strclass = '';
+var strClass = '';
 var res = '';
+
 $("tbody > tr").each(function() {
 
     if ($(this).attr("title") == undefined) {
         return;
     }
 
-    if (strclass == '') {
-        strclass = $(this).attr("title");
-        res = strclass.split(" ");
+    if (strClass == '') {
+        strClass = $(this).attr("title");
+        res = strClass.split(" ");
         return;
     }
 
-    let strclasscheck = $(this).attr("title");
-    let rescheck = strclasscheck.split(" ");
+    let strClassCheck = $(this).attr("title");
+    let resCheck = strClassCheck.split(" ");
 
-    if (rescheck.includes(res[res.length - 1])) {
+    if (resCheck.includes(res[res.length - 1])) {
         $(this).attr("style","visibility: collapse");
     }
     else {
-        strclass = $(this).attr("title");
-        res = strclass.split(" ");
+        strClass = $(this).attr("title");
+        res = strClass.split(" ");
     }
 
 });
 
-$("table.collapsible thead").find("th").on("click", function() {
+$("table.collapsible thead").find("th").on("click", function() 
+{
+
     $(this).get(0).className = ($(this).get(0).className == 'open') ? 'closed' : 'open';
     $(this).closest("table").find("tbody").toggle();
+
 });
 
-$("tr").find("td.path").on("click",function(event) {
+$("tr").find("td.path").on("click",function(event) 
+{
 
     event.stopPropagation();
 
     let $target = $(event.target);
-    let strclass = $target.closest("tr").attr("title");
-    let res = strclass.split(" ");
+    let strClass = $target.closest("tr").attr("title");
+    let res = strClass.split(" ");
 
     if ($target.closest("tr").next().attr("title") == undefined) {
         return;
     }
 
-    let strclasscheck = $target.closest("tr").next().attr("title");
-    let rescheck = strclasscheck.split(" ");
-    let performaction = $target.closest("tr").attr("customaction") == 'close'? 'open' : 'close';
+    let strClassCheck = $target.closest("tr").next().attr("title");
+    let resCheck = strClassCheck.split(" ");
+    let performAction = $target.closest("tr").attr("customaction") == 'close'? 'open' : 'close';
 
-    $target.closest("tr").attr("customaction",performaction);
+    $target.closest("tr").attr("customaction",performAction);
 
-    while (rescheck.includes(res[res.length - 1])) {
-        if ($target.closest("tr").next().attr("customaction") != performaction) {
-            $target.closest("tr").next().attr("customaction",performaction);
+    while (resCheck.includes(res[res.length - 1])) {
+        if ($target.closest("tr").next().attr("customaction") != performAction) {
+            $target.closest("tr").next().attr("customaction",performAction);
 
-            if (performaction == 'open') {
+            if (performAction == 'open') {
                 $target.closest("tr").next().attr("style","visibility: visible");
-                $target.closest("tr").next().attr("customaction",performaction);
+                $target.closest("tr").next().attr("customaction",performAction);
             }
             else {
                 $target.closest("tr").next().attr("style","visibility: collapse");
-                $target.closest("tr").next().attr("customaction",performaction);
+                $target.closest("tr").next().attr("customaction",performAction);
             }
         }
         else {
 
-            if (performaction == 'open') {
+            if (performAction == 'open') {
                 $target.closest("tr").next().attr("style","visibility: visible");
             }
             else {
@@ -98,8 +115,8 @@ $("tr").find("td.path").on("click",function(event) {
             break;
         }
         else {
-            strclasscheck = $target.closest("tr").next().attr("title");
-            rescheck = strclasscheck.split(" ");
+            strClassCheck = $target.closest("tr").next().attr("title");
+            resCheck = strClassCheck.split(" ");
         }
     }
 
