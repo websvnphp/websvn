d1b27a97576162026f6972ea33122c0348caf384
Thorsten Schöning <tschoening@am-soft.de>
2021-05-12 09:52:54 +0200

    Delete temporary directories automatically.
    When downloading files, temporary directories are created and ONLY get removed in case the download succeeds. Though, one can't influence if this happens or not, e.g. in case of requests by search bots, which might simply close connections in case of unexpected content. This might leave lots of temporary dirs and files behind.
    
    This fixes https://github.com/websvnphp/websvn/issues/141.

--- include/utils.php
+++ include/utils.php
@@ -399,22 +399,69 @@ function getUserLanguage($languages, $default, $userchoice) {
 
 // }}}
 
-// {{{ tempnameWithCheck
+// {{{ removeDirectory
 
-function tempnamWithCheck($dir, $prefix) {
-	$tmp = tempnam($dir, $prefix);
+function removeDirectory($dir)
+{
+	if (!is_dir($dir))
+	{
+		return false;
+	}
 
-	if ($tmp == false) {
-		if (!headers_sent()) {
-			http_response_code(500);
-			error_log('Unable to create a temporary file. Either make the currently used directory ("' . $dir . '") writable for WebSVN or change the directory in the configuration.');
-			print 'Unable to create a temporary file. Either make the currently used directory writable for WebSVN or change the directory in the configuration.';
-			exit(0);
-		} else {
-			global $vars;
-			$vars['warning'] = 'Unable to create a temporary file. Either make the currently used directory writable for WebSVN or change the directory in the configuration.';
+	$dir	= rtrim($dir, '/');
+	$handle	= dir($dir);
+
+	while (($file = $handle->read()) !== false)
+	{
+		if (($file == '.') || ($file == '..'))
+		{
+			continue;
+		}
+
+		$f = $dir.DIRECTORY_SEPARATOR.$file;
+		if (!is_link($f) && is_dir($f))
+		{
+			removeDirectory($f);
+		}
+		else
+		{
+			@unlink($f);
 		}
 	}
+
+	$handle->close();
+	@rmdir($dir);
+
+	return true;
+}
+
+// }}}
+
+// {{{ tempnameWithCheck
+
+function tempnamWithCheck($dir, $prefix, $rmOnShutdown = true) {
+	$tmp = tempnam($dir, $prefix);
+
+	if ($tmp && !$rmOnShutdown)
+	{
+		return $tmp;
+	}
+	if ($tmp && $rmOnShutdown)
+	{
+		register_shutdown_function(removeDirectory, $tmp);
+		return $tmp;
+	}
+
+	if (!$tmp && !headers_sent())
+	{
+		http_response_code(500);
+		error_log('Unable to create a temporary file. Either make the currently used directory ("' . $dir . '") writable for WebSVN or change the directory in the configuration.');
+		print 'Unable to create a temporary file. Either make the currently used directory writable for WebSVN or change the directory in the configuration.';
+		exit(0);
+	}
+
+	global $vars;
+	$vars['warning'] = 'Unable to create a temporary file. Either make the currently used directory writable for WebSVN or change the directory in the configuration.';
 
 	return $tmp;
 }
